edb26ff66890c05f2116a145ba869928ca23c7a7
Antfather <925627037@qq.com>
2018-06-12 02:06:35 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(".bannerbox").slide({
+    titCell: ".yuandian li",
+    mainCell: ".bd",
+    autoPlay: 'true',
+    depayTime: 2500,
+    effect: 'leftLoop'
+});
+
+$(".project").slide({
+    mainCell: ".item",
+    effect: "leftLoop",
+	interTime:2500,
+	vis:4,
+    autoPlay: 'true',
+});
